cb88dc9388cec9e5bd65ef57cf07741d4e073d13
Sean Midford <seanmidford@gmail.com>
2015-03-04 02:41:06 -0600

    Added list view for library (with dummy entries).

--- Decentralized-Library/app/src/main/java/com/comp4020/decentralized_library/MainActivity.java
+++ Decentralized-Library/app/src/main/java/com/comp4020/decentralized_library/MainActivity.java
@@ -5,18 +5,16 @@ import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity
@@ -27,6 +25,8 @@ public class MainActivity extends Activity
      */
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
+    private ListView listView;
+
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
      */
@@ -36,6 +36,37 @@ public class MainActivity extends Activity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize listView items
+        listView = (ListView) findViewById(R.id.listView);
+
+        String[] titles = new String[] { "Dresden Files Skin Game", "Watchmen", "V For Vendetta"};
+        String[] authors = new String[] { "Jim Butcher", "Alan Moore", "Alan Moore"};
+        String[] covers = new String[] { "djskalfj", "huerdiop", "jowlkcui"};
+
+        final LibraryArrayAdapter adapter = new LibraryArrayAdapter(this, R.layout.row_layout,
+                titles, authors, covers);
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+
+                final String item = (String) parent.getItemAtPosition(position);
+                view.animate().setDuration(20).alpha(0)
+                        .withEndAction(new Runnable() {
+                            @Override
+                            public void run() {
+                                //list.remove(item);
+                                //adapter.notifyDataSetChanged();
+                                //view.setAlpha(1);
+                            }
+                        });
+            }
+
+        });
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getFragmentManager().findFragmentById(R.id.navigation_drawer);
@@ -143,5 +174,4 @@ public class MainActivity extends Activity
                     getArguments().getInt(ARG_SECTION_NUMBER));
         }
     }
-
 }
